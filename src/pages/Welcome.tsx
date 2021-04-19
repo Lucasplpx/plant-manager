526cd2e5c83180cf3ca37f5c7d060edbb141579b
Lucas Passos <lucasplpx@gmail.com>
2021-04-19 20:49:07 -0300

    01 - Liftoff

---
+++ src/pages/Welcome.tsx
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+import {
+  SafeAreaView,
+  Text,
+  Image,
+  StyleSheet,
+  Platform,
+  StatusBar,
+} from 'react-native';
+import colors from '../../styles/colors';
+
+import wateringImg from '../assets/watering.png';
+import Button from '../components/Button';
+
+const Welcome: React.FC = () => {
+  const [visible, setVisible] = useState(false);
+
+  function handleVisibility() {
+    setVisible(!visible);
+  }
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>
+        Gerencie {'\n'}
+        suas plantas {'\n'}
+        de forma fácil
+      </Text>
+      <Image source={wateringImg} style={styles.image} />
+      <Text style={styles.subTitle}>
+        Não esqueça mais de regar suas plantas. Nós cuidamos de lembrar você
+        sempre que precisar.
+      </Text>
+
+      <Button title="&gt;" onPress={handleVisibility} />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: colors.green,
+    marginTop: 38,
+  },
+  subTitle: {
+    textAlign: 'center',
+    fontSize: 18,
+    paddingHorizontal: 20,
+    color: colors.heading,
+  },
+  image: {
+    width: 292,
+    height: 284,
+  },
+});
+
+export default Welcome;
